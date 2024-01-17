32451355f618260fae40ec101d81d09b552cbfe1
Chandan <100086571+chandansy@users.noreply.github.com>
2024-01-17 08:43:29 +0530

    week 2 and 3

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,65 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+
+
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+	let categories_list = [];
+	let total_spent = {};
+	for (let i = 0; i < transactions.length; i++) {
+		categories_list.push(transactions[i]['category']);
 }
+	categories_list = [...new Set(categories_list)];
+	
+	categories_list.forEach(element => {
+		for (let i = 0; i< transactions.length; i++) {
+			if (transactions[i]['category'] === element) {
+				total_spent[element] = transactions[i]['price'];
+			}
+	});
+}
+
+
+calculateTotalSpentByCategory([
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Travel',
+		itemName: 'Pizza',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Entertainment',
+		itemName: 'Pizza',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Rent',
+		itemName: 'Pizza',
+	}
+]);
+
+
+
 
 module.exports = calculateTotalSpentByCategory;
---
+++ week-2/01-async-js/easy/counter.js
@@ -0,0 +1,13 @@
+// Create a counter in JavaScript. It should go up as time goes by in intervals of 1 second
+
+fubction counter() {
+    let count = 0;
+    setInterval(function() {
+        count++;
+        console.log(count);
+    }, 1000);
+    }
+
+
+    
+
