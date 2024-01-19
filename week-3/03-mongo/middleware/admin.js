b5244ac6ec4c6eeff35fe77046d6b6d6f86dc12b
Chandan <100086571+chandansy@users.noreply.github.com>
2024-01-19 08:09:37 +0530

    week 3 answers

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,25 +1,25 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
-const {admin, Admin} = require('../db');
-
-
-
-
-
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-    const username = req.headers.username;
-    const password = req.headers.password;
+    const username = req.headers.username; // harkirat@gmail.com
+    const password = req.headers.password; /// 123456
+
     Admin.findOne({
         username: username,
         password: password
-    }).then((admin) => {
-        if (admin) {
+    })
+    .then(function(value) {
+        if (value) {
             next();
         } else {
-            res.status(403).json({message: "Admin deosn't exist"});
+            res.status(403).json({
+                msg: "Admin doesnt exist"
+            })
         }
     })
 }
- 
+
 module.exports = adminMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -6,19 +6,40 @@ const {Admin}  = require('../db/index');
 // Admin Routes
 router.post('/signup', async (req, res) => {
     // Implement admin signup logic
-    const username = req.body.username; 
+    const username = req.body.username;
     const password = req.body.password;
 
-    await Admin.create({
+    // check if a user with this username already exists
+    const admin_created = await Admin.create({
         username: username,
         password: password
     })
-    res.json({msg: "Admin created successfully"});
+    console.log(admin_created)
+    res.json({
+        message: 'Admin created successfully'
+    })
     
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+
+
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    // zod
+    const newCourse = await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+
+    res.json({
+        message: 'Course created successfully', courseId: newCourse._id
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
